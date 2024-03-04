e29729f40b4a227c024810992e00099ea68e7a4f
Vladimir Jakubik <vladimir.jakubik77@gmail.com>
2024-03-04 14:25:48 +0100

    Create component Entry and use react map() to loop through each item or array to show on page

--- src/emojipedia.js
+++ src/emojipedia.js
@@ -19,5 +19,13 @@ const emojipedia = [
     name: "Rolling On The Floor, Laughing",
     meaning:
       "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
-  }
+  },
+  {
+    id: 4,
+    emoji: "🥹",
+    name: " Face Holding Back Tears",
+    meaning:
+      "This emoji shows a slightly smiling face with tears in its eyes, can express various emotions such as gratitude, emotion, or pride. At the same time, anger or rage can also be the reason why tears can hardly be held back. It is often used to express strong feelings in communication, and the emotional interpretation depends on the context. In conversation, this emoji can be used to convey empathy and compassion, especially when discussing emotional topics. It is also used to express our deep feelings in an authentic and touching way, emphasizing the human side of our communication."
+  },
 ];
+export default emojipedia;
